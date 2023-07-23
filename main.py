490334d10fb8fff6a9b52258c104d7f589223cb8
ArtemGribov <gribov.ag@gmail.com>
2023-07-23 15:25:14 +0300

    test1 push 2

---
+++ main.py
@@ -0,0 +1,10 @@
+#57
+#Поиск индекса наименьшего значения элемента массива
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
